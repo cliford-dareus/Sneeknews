08b517ad4507fcc0e35dec867eb33f4826146a1c
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2022-09-04 18:51:52 -0400

    Update Stories.js
    finally got the filter system to work

--- Pages/Stories.js
+++ Pages/Stories.js
@@ -1,31 +1,20 @@
 import newsSection from "../Components/newsSection";
 import showCase from "../Components/showCase";
-import view from "../Utils/view"
+import view from "../Utils/view";
 
-const index = 2
+let stories = null;
+const index = 0;
+
 
 export default async function Stories(path){
-    const stories = await getStories(path);
+    stories = await getStories(path);
     const hasStories = stories.length > 0;
-
     let filterStories = stories;
 
-    render(hasStories, filterStories)
-
-    console.log(stories)
-
-   const filterBtn = document.querySelectorAll('.filter_btn');
-    filterBtn.forEach(btn => btn.addEventListener('click', (e) => {
-        const filter = e.target.innerHTML;
-        
-        if(filter === 'All'){
-            render(hasStories, filterStories)
-        }
-    }));
+    render(hasStories, filterStories);
 };
 
 function render(hasStories, filterStories){
-    console.log('hello')
     view.innerHTML = `<section>
         <div class="showcase">
             ${hasStories ? showCase(filterStories[index], index) : 'No stories'}
@@ -40,6 +29,27 @@ function render(hasStories, filterStories){
             ${hasStories ? filterStories.map(story => newsSection(story)).join('') : 'No stories'}
         </div>
    </section>`
+
+   handleFilter(hasStories,filterStories)
+}
+
+function handleFilter(hasStories, filterStories){
+    const filterBtn = document.querySelectorAll('.filter_btn');
+    filterBtn.forEach(btn => btn.addEventListener('click', (e) => {
+        const filter = e.target.innerHTML;
+        
+        if(filter === 'All'){
+            render(hasStories, stories)
+        }else if(filter === 'Ask'){
+            const askFilter = filterStories.filter(story => story.type === 'ask')
+            askFilter.length ===0 ? render(hasStories, stories): 
+            render(hasStories, askFilter);
+        }else if(filter === 'Links'){
+            const linkFilter = filterStories.filter(story => story.type === 'link')
+            linkFilter.length === 0 ? render(hasStories, stories): 
+            render(hasStories, linkFilter);
+        }
+    }));
 }
 
 async function getStories(path){
